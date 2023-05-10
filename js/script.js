36ebb84d52a7af3d4f457d7322c5eab9702ff2e9
Pritam Samui <77156502+Pritamart@users.noreply.github.com>
2023-05-10 17:39:52 +0530

    script.js

--- js/script.js
+++ js/script.js
@@ -18,12 +18,29 @@ const sr = ScrollReveal ({
     duration: 2700,
     reset: true
 })
+const ssr = ScrollReveal ({
+    distance: '45px',
+    duration: 1500,
+    reset: true
+})
+
 sr.reveal('.home-text', {delay:350, origin: 'left'})
 sr.reveal('.home-img', {delay:350, origin: 'right'})
+sr.reveal('.about-text', {delay:350, origin: 'left'})
+sr.reveal('.about-img', {delay:350, origin: 'right'})
 sr.reveal('#skill-section', {delay:50, origin: 'left'})
 
 
-sr.reveal('.home-img, .about, .projects, .contact-me, .col', {delay:200, origin: 'bottom'})
+sr.reveal('.contact-me,.contacUS', {delay:200, origin: 'bottom'})
+sr.reveal('.col:nth-child(1)', {delay:80, origin: 'left'})
+sr.reveal('.col:nth-child(2)', {delay:80, origin: 'right'})
+sr.reveal('.col:nth-child(3)', {delay:160, origin: 'left'})
+sr.reveal('.col:nth-child(4)', {delay:160, origin: 'right'})
+sr.reveal('.col:nth-child(5)', {delay:240, origin: 'left'})
+sr.reveal('.col:nth-child(6)', {delay:240, origin: 'right'})
+
+
+
 
 
 const typed = new Typed('.multiple-text', {
@@ -33,3 +50,18 @@ const typed = new Typed('.multiple-text', {
     backDelay: 1000,
     loop: true
 });
+
+// skils js 
+var forEach = function (array, callback, scope) {
+    for (var i = 0; i < array.length; i++) {
+      callback.call(scope, i, array[i]);
+    }
+  };
+  window.onload = function(){
+    var max = -219.99078369140625;
+    forEach(document.querySelectorAll('.progress'), function (index, value) {
+    percent = value.getAttribute('data-progress');
+      value.querySelector('.fill').setAttribute('style', 'stroke-dashoffset: ' + ((100 - percent) / 100) * max);
+      value.querySelector('.value').innerHTML = percent + '%';
+    });
+  }
